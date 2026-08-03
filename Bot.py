6fcdd9a66b4a910e6f923e4df0ed975ee3389b66
Lucas <88356512+LuContar@users.noreply.github.com>
2026-08-03 15:42:30 -0300

    Bot.py

--- Bot.py
+++ Bot.py
@@ -127,7 +127,7 @@ def main():
     app.add_handler(CallbackQueryHandler(responder_boton))
 
     print("Bot de cauciones ejecutándose...")
-    app.run_polling()
+    app.run_polling(drop_pending_updates=True)
 
 if __name__ == '__main__':
     main()
